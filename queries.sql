fbbb4cffe068c17e8d3dc14e5c1f470aabcc04e4
Danny Le <le.dannyj@gmail.com>
2021-04-27 13:02:41 -0400

    created sql database and template for employees

---
+++ queries.sql
@@ -0,0 +1,41 @@
+createdb CompanyDatabase
+
+pgcli CompanyDatabase
+
+CompanyDatabase> CREATE TABLE "Empoyees" (
+................ "FullName" TEXT,
+................ "Salary" INT,
+................ "JobPosition" TEXT,
+................ "PhoneExtension" INT,
+................ "IsPartTime" TEXT
+................ );
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('', , '', , '');
