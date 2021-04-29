11600b83f6fefe5b933d16b575ec4a98c4cb4e10
Danny Le <le.dannyj@gmail.com>
2021-04-29 16:04:34 -0400

    info for music database added

--- queries.sql
+++ queries.sql
@@ -1,3 +1,194 @@
+
+
+createdb MusicDatabase
+
+pgcli MusicDatabase
+
+
+
+
+CREATE TABLE "Bands" (
+  "Id"	SERIAL PRIMARY KEY,
+  "Name"  TEXT,
+  "CountryOfOrigin"  TEXT,
+  "NumberOfMembers"  INT,
+  "Website" TEXT,
+  "Style" TEXT,
+  "IsSigned" TEXT,
+  "ContactName" TEXT,
+  "ContactPhoneNumber" INT
+);
+
+
+CREATE TABLE "Albums" (
+  "Id"	SERIAL PRIMARY KEY,
+  "Title"  TEXT,
+  "IsExplicit"  TEXT,
+  "ReleaseDate"  DATE
+);
+
+CREATE TABLE "Songs" (
+  "Id"	SERIAL PRIMARY KEY,
+  "TrackNumber"  INT,
+  "Title"  TEXT,
+  "Duration"  TIME
+);
+
+-- ADD THIS ONCE YOU HAVE INSERTED ALL BANDS
+ALTER TABLE "Albums" ADD COLUMN "BandId" INTEGER NULL REFERENCES "Bands" ("Id");
+
+
+-- ADD THIS ONCE YOU HAVE INSERTED ALL ALBUMS
+ALTER TABLE "Songs" ADD COLUMN "AlbumId" INTEGER NULL REFERENCES "Albums" ("Id");
+
+
+--INSERT INTO "Bands" ("Name",  "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber")
+--VALUES ( '', '', , '', '', '', '',);
+
+--INSERT INTO "Albums" ("Title",  "IsExplicit", "ReleaseDate", "BandId")
+--VALUES ( '', '', '',);
+
+--INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+--VALUES ( , '', '00:0:', );
+
+
+
+INSERT INTO "Bands" ("Name",  "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber")
+VALUES ( 'Little Mix', 'England', 3, 'little-mix.com', 'Pop', 'Yes', 'Samantha Cox', 5555);
+
+-- ADD BandId TO ALL ALBUMS
+INSERT INTO "Albums" ("Title",  "IsExplicit", "ReleaseDate", "BandId")
+VALUES ( 'Confetti', 'No', '11/06/2020', 1);
+
+-- ADD AlbumId TO ALL SONGS
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 1, 'Break Up Song', '00:03:20', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 2, 'Holiday', '00:03:33', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 3, 'Sweet Melody', '00:03:33', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 4, 'Confetti', '00:02:47', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 5, 'Happiness', '00:03:17', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 6, 'Not a Pop Song', '00:02:59', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 7, 'Nothing But My Feelings', '00:02:42', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 8, 'Gloves Up', '00:02:47', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 9, 'A Mess (Happy 4 U)', '00:03:29', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 10, 'My Love Wont Let You Down', '00:02:54', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 11, 'Rendezvous', '00:02:56', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 12, 'If You Want My Love', '00:02:40', 1);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 13, 'Breathe', '00:03:29', 1);
+
+
+
+
+INSERT INTO "Bands" ("Name",  "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber")
+VALUES ( 'Ariana Grande', 'USA', 1, 'arianagrande.com', 'Pop/R&B', 'Yes', 'Scooter Braun', 666);
+
+
+
+INSERT INTO "Albums" ("Title",  "IsExplicit", "ReleaseDate", "BandId")
+VALUES ( 'Positions', 'Yes', '10/30/2020', 2);
+
+
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 1, 'Just like Magic', '00:02:29', 2);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 2, 'Off the Table (with the Weeknd)', '00:03:59', 2);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 3, 'Safety Net (ft. Ty Dolla Sign)', '00:03:28', 2);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 4, 'West Side', '00:02:12', 2);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 5, 'Positions', '00:02:52', 2);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 6, 'Obvious', '00:02:28', 2);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 7, 'POV', '00:03:21', 2);
+
+
+
+
+
+INSERT INTO "Bands" ("Name",  "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber")
+VALUES ( 'BLACKPINK', 'Korea', 4, 'blackpinkofficial.com', 'K-Pop', 'No', 'YG Entertainment', 7777);
+
+
+
+
+INSERT INTO "Albums" ("Title",  "IsExplicit", "ReleaseDate", "BandId")
+VALUES ( 'The Album', 'Yes', '10/02/2020', 3);
+
+
+
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 1, 'How You Like That', '00:03:01', 3);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 2, 'Ice Cream (with Selena Gomez)', '00:02:56', 3 );
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 3, 'Pretty Savage', '00:03:19', 3);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 4, 'Bet You Wanna (ft. Cardi B)', '00:02:39', 3);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 5, 'Lovesick Girls', '00:03:12', 3);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 6, 'Crazy Over You', '00:02:41', 3);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 7, 'Love to Hate Me', '00:02:49', 3);
+
+INSERT INTO "Songs" ("TrackNumber",  "Title", "Duration", "AlbumId")
+VALUES ( 8, 'You Never Know', '00:03:49', 3);
+
+
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+
 createdb CompanyDatabase
 
 pgcli CompanyDatabase
